2054bfea1a3b2508966d04a372e00102635808ba
Serendipity <112377744+39998@users.noreply.github.com>
2023-12-08 20:38:41 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,2 @@
+sudo curl -fsSL http://file.tickstep.com/apt/pgp | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/tickstep-packages-archive-keyring.gpg > /dev/null && echo "deb [signed-by=/etc/apt/trusted.gpg.d/tickstep-packages-archive-keyring.gpg arch=amd64,arm64] http://file.tickstep.com/apt aliyunpan main" | sudo tee /etc/apt/sources.list.d/tickstep-aliyunpan.list > /dev/null && sudo apt-get update && sudo apt-get install -y aliyunpan
+ 
